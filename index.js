f9617dcba7ddcba36334bc2aa8f029c206f6e996
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-19 01:28:05 -0500

    fix path prefix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var isWindows = require('is-windows');
-var root = require('global-modules');
-var unique = require('array-unique');
 var path = require('path');
+var root = require('global-modules');
+var isWindows = require('is-windows');
+var unique = require('array-unique');
 
 /**
  * Returns an array of directories to use as a starting
@@ -20,7 +20,7 @@ module.exports = function paths(cwd) {
   var segs = cwd.split(path.sep);
 
   while (segs.pop()) {
-    res.push((isWindows() ? '/' : '') + npm(segs.join('/')));
+    res.push((!isWindows() ? '/' : '') + npm(segs.join('/')));
   }
 
   if (process.env.NODE_PATH) {
